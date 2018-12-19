77c04db75235ce4ffb6918a0b2cd2eeac9547d4a
Tsion Adefres <tadefres@gmail.com>
2018-12-18 21:31:44 -0800

    Adding new routes

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 
+const Painting = require('./models/Painting');
+
 
 // Mongoose configuration
 mongoose.Promise = global.Promise; // Allows use of native promises with mongoose
 mongoose
   .connect(
     process.env.DB_URI, // DB URI from .env file
-    { useNewUrlParser: true } // Resolves deprecation warning
+    { useNewUrlParser: true }, // Resolves deprecation warning
   )
   .then(console.log('Connected to database.'));
 
@@ -20,13 +22,34 @@ const server = hapi.server({
 });
 
 const init = async () => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(req, res) {
-      return `<h1> My Hapi Swagger modern api </h1>`;
-    }
-  });
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: (req, res) => {
+        return `<h1> My Hapi Swagger modern api </h1>`;
+      },
+    },
+    {
+      method: 'GET',
+      path: '/api/v1/paintings',
+      handler: (req, res) => Painting.find(),
+    },
+    {
+      method: 'POST',
+      path: '/api/v1/paintings',
+      handler: (req, res) => {
+        const { name, url, techniques } = req.payload;
+        const painting = new Painting({
+          name,
+          url,
+          techniques,
+        });
+
+        return painting.save();
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server is running at: ${server.info.uri}`);
