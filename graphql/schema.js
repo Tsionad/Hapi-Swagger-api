2ee1c6e3a6cd4e98aa59b1fd1f93dd08c52187cb
Tsion Adefres <tadefres@gmail.com>
2018-12-18 23:05:00 -0800

    Created graphql schema file

---
+++ graphql/schema.js
@@ -0,0 +1,10 @@
+const graphql = require('graphql');
+const PaintingType = require('./PaintingType');
+const Painting = require('./../models/Painting');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+} = graphql;
+
